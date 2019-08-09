95ae9871cd1036fe99efc7b781f182987e524013
Your Name <pankajbhadauriya121@gmail.com>
2019-08-09 11:15:05 +0530

    'typescript-minifycssplugin'

---
+++ README.md
@@ -0,0 +1,13 @@
+# react-todo-list
+
+## Install
+
+To install the dependencies
+```
+$ npm i
+```
+
+To run the application
+```
+$ npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  target: 'web',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: true
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  }
+};
